6e1e668f4daa05794292833c7c45b077201c0901
Dorahely Sanchez <dorahelys@gmail.com>
2023-10-31 10:14:28 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
+//Create a variable called multiply set to an equation that will multiply the variables num1 and num2; the result of the multiplication should be 62.
+var num1 = 2
+var num2 = 31
+var multiply = num1 * num2
 
-// Write your code here
+//if i want to tes use node index.js or can go ive index.html
+//console.log(multiply)
+/*
+// does this work??
+function multiply(num1, num2){
+const number = num1*num2
+return number
+}
+*/
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+
+let num3 = 18
+let num4 = 7
+let mod = num3 % num4
+
+let max = Math.max(12,2,6,20)
